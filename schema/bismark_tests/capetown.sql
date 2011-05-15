7a63a35e1a1970d93f1d91ec26eb24aab1029b20
Srikanth Sundaresan <srikanth@galapagos.(none)>
2011-05-15 13:42:26 -0400

    Reorganized for capetown

---
+++ schema/bismark_tests/capetown.sql
@@ -0,0 +1,19 @@
+\i defaults.sql
+
+CREATE TABLE aggl3bitrate (like pings including defaults including constraints including indexes);
+CREATE TABLE bitrate (like pings including defaults including constraints including indexes);
+CREATE TABLE capacity (like pings including defaults including constraints including indexes);
+CREATE TABLE jitter (like pings including defaults including constraints including indexes);
+CREATE TABLE lmrtt (like pings including defaults including constraints including indexes);
+CREATE TABLE pktloss (like pings including defaults including constraints including indexes);
+CREATE TABLE rtt (like pings including defaults including constraints including indexes);
+CREATE TABLE shaperate (like pings including defaults including constraints including indexes);
+CREATE TABLE ulrttdw (like pings including defaults including constraints including indexes);
+CREATE TABLE ulrttup (like pings including defaults including constraints including indexes);
+
+CREATE TABLE dnsdelay (like pings including defaults including constraints including indexes);
+CREATE TABLE dnsdelayc (like pings including defaults including constraints including indexes);
+CREATE TABLE dnsdelaync (like pings including defaults including constraints including indexes);
+CREATE TABLE dnsfail (like pings including defaults including constraints including indexes);
+CREATE TABLE dnsfailc (like pings including defaults including constraints including indexes);
+CREATE TABLE dnsfailnc (like pings including defaults including constraints including indexes);
--- schema/bismark_tests/dns.sql
+++
@@ -1,6 +0,0 @@
-\i defaults.sql
-
---  example
-
-CREATE TABLE dns (like pings including defaults including constraints including indexes);
-
