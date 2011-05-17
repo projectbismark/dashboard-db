1932d4b9ecefbd83b3ea816c32c109bb19991e8d
Dave Taht <d+git@taht.net>
2011-05-16 18:41:57 -0600

    Converted the event stamp to an honest timestamp.
    seconds since the epoch doesn't work for me.

--- schema/types.sql
+++ schema/types.sql
@@ -1,7 +1,15 @@
 \i defaults.cfg
+
+-- goof
+
+--UPDATE - 2010-08-21 as noted by a user below. In newer versions to_timestamp is shorter and easier:
+--SELECT to_timestamp(1195374767);
+--To convert back to unix timestamp you can use date_part:
+--SELECT date_part('epoch',CURRENT_TIMESTAMP)::int 
+
 create domain deviceid_t as macaddr not null;
 create domain deviceidref_t as macaddr;
-create domain eventstamp_t as numeric(20,0) DEFAULT NULL::numeric NOT NULL;
+create domain eventstamp_t as timestamp;
 create domain macaddr_t as macaddr;
 create domain osname_t as varchar(10) DEFAULT NULL::varchar;
 create domain version_t as varchar(10) DEFAULT NULL::varchar;
@@ -13,29 +21,21 @@ create domain essid_t as varchar(32);
 create domain ssid_t as varchar(32) NOT NULL;
 create domain inetn_t as inet NOT NULL;
 create domain dnsname_t as varchar(255);
-create domain dhcp_action_t varchar(20) DEFAULT NULL::character varying NOT NULL;
+create domain dhcp_action_t varchar(4) DEFAULT NULL;
 create domain email_t varchar(80) DEFAULT NULL;
 
--- create domain id_t as bigint NOT NULL; -- trigger needed
--- create domain idref_t as bigint; -- trigger neededd
+-- NULL is handled by the trigger in this case.
 
--- create domain id_t as bigserial; -- no trigger needed, but this notational
--- create domain idref_t as bigint;
-
--- better would be (perhaps enforce not null here, too)
-
-create domain sha1_t as bytea; --  check(length(VALUE) = 20);
+create domain sha1_t as bytea;
 create domain sha1ref_t as bytea;
-
-create domain id_t as sha1_t; -- NOT NULL; -- with trigger needed to generate
+create domain id_t as sha1_t; 
 create domain idref_t as sha1ref_t; 
-
-create domain toolid_t as sha1_t; -- NOT NULL; -- with trigger needed to generate
+create domain toolid_t as sha1_t; 
 create domain toolidref_t as sha1ref_t; 
-
-create domain testseries_t as sha1_t NOT NULL; -- with trigger needed to generate
+create domain testseries_t as sha1_t ; 
 create domain testseriesref_t as sha1ref_t; 
 
+-- Need some regexes for other validation
 -- This is old syntax and I can't remember the new syntax
 -- CREATE DOMAIN us_postal_code AS TEXT
 -- CHECK(
